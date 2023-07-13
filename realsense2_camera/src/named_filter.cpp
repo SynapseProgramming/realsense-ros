1c5b7ca29cba3fd28a65881730c3009defc9935b
Roald Ong <roald.ong@gmail.com>
2023-07-13 15:43:04 +0800

    Added in voxel filter

--- realsense2_camera/src/named_filter.cpp
+++ realsense2_camera/src/named_filter.cpp
@@ -285,6 +285,17 @@ void PointcloudFilter::Publish(rs2::points pc, const rclcpp::Time& t, const rs2:
         // TODO: add in voxel grid filter here
 
     // sensor_msgs::msg::PointCloud2::UniquePtr msg_pointcloud = std::make_unique<sensor_msgs::msg::PointCloud2>();
+
+    pcl_conversions::toPCL(*msg_pointcloud, pclcloud);
+
+    pcl::PCLPointCloud2ConstPtr inpPclPtr =
+        std::make_shared<pcl::PCLPointCloud2>(pclcloud);
+    voxelfilter.setInputCloud(inpPclPtr);
+    voxelfilter.setLeafSize(0.04f, 0.04f, 0.04f);
+    voxelfilter.filter(pclcloud);
+    pcl_conversions::fromPCL(pclcloud, *msg_pointcloud);
+
+
         std::lock_guard<std::mutex> lock_guard(_mutex_publisher);
         if (_pointcloud_publisher)
             _pointcloud_publisher->publish(std::move(msg_pointcloud));
